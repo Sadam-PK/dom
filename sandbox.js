2ee0ab1b300bb680b16669eebf26350a4fba2922
sadam <sadamccr@gmail.com>
2023-01-18 00:43:42 +0500

    querySelector - done

---
+++ sandbox.js
@@ -0,0 +1,6 @@
+// const para = document.querySelector('.error')
+// const para = document.querySelector('div.error')
+// console.log(para);
+
+const para = document.querySelectorAll('p')
+console.log(para);
