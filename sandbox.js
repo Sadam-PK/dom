ffb3bd7d5e1f2a0b9a4fbebaf6805293c938f9af
sadam <sadamccr@gmail.com>
2023-01-18 01:03:40 +0500

    by id and class name done

--- sandbox.js
+++ sandbox.js
@@ -2,5 +2,14 @@
 // const para = document.querySelector('div.error')
 // console.log(para);
 
-const para = document.querySelectorAll('p')
-console.log(para);
+// const para = document.querySelectorAll('p')
+// console.log(para);
+
+
+// const title = document.getElementById('page-title')
+// console.log(title);
+
+
+// const paras = document.getElementsByClassName('paras')
+// console.log(paras);
+
